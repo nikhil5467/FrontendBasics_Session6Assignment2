66d05ceaf283178a630bb165f56333602901ec0e
nikhil5467 <nikhilrocks374@gmail.com>
2016-09-14 02:11:52 +0530

    session6assignment2

---
+++ app5.js
@@ -0,0 +1,21 @@
+var num1 = 5;
+var num2 = 2;
+var num3 =8;
+console.log("First number is ="+num1);
+console.log("Second number is ="+num2);
+console.log("Third number is ="+num3);
+if(num1>num2 && num1>num3){
+    console.log(num1 + " is the largest of the three numbers");
+    
+}
+else if(num2>num1 && num2>num3){
+  console.log(num2 + " is the largest of the three numbers");  
+    
+}
+else{
+    
+  console.log(num3 + " is the largest of the three numbers");    
+}
+
+
+
